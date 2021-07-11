e18a1c704da154aa398798bb989d2c14128173e0
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-11 15:26:06 +0300

    register my models

--- cloned/admin.py
+++ cloned/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Follow,Stream,tags,Post,Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(tags)
+admin.site.register(Stream)
+admin.site.register(Follow)
+admin.site.register(Post)
+
+
