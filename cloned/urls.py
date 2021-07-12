b75874b942a074cdfa17f1a87fea6cc899929f5f
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-12 23:46:59 +0300

    my urls landinging and other insta pages

--- cloned/urls.py
+++ cloned/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 urlpatterns = [
     # path('', views.insta, name="insta"),
-    path('', views.index, name="index"),
-
-
+    path('', views.loginPage, name='login'),
+    path('index/', views.index, name="index"),
+    path('newspost/', views.NewPost, name="newspost"),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+    # path('like/<int:pk>', views.BlogPostLike, name='like_post')
 ]
 
 if settings.DEBUG:
