5d3a417cabda94c0762b68ab734db1f20c585717
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-14 12:11:18 +0300

    my admin set

--- cloned/admin.py
+++ cloned/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Follow,Stream,tags,Post,Profile
+from .models import Follow,tags,Post,Profile
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(tags)
-admin.site.register(Stream)
 admin.site.register(Follow)
 admin.site.register(Post)
 
--- cloned/forms.py
+++ cloned/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post, Profile
 from django.contrib.auth.models import User
+
 from django.contrib.auth.forms import UserCreationForm
 
 class NewsPostForm(forms.ModelForm):
@@ -21,15 +22,21 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
 class UpdateForm(forms.ModelForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username','email']
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['picture','profile_info']
+
+# class CommentForm(forms.ModelForm):
+
+    
 
 
 
